fde690868ca34b3d3a1afacf6594c29aa0533ce9
Louisa Murray <whiringa+1@gmail.com>
2024-12-28 18:03:05 +1300

    Edit layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import './i18n';
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../../src/il8n'; 
-
+import i18n from '../il8n';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
